8202626bb25f3034f85d407e95d206340b29dc93
arthur <shuiyuzhe@gmail.com>
2017-10-12 22:02:48 +0800

    begin to run

--- qingcloud/builder.go
+++ qingcloud/builder.go
@@ -1,10 +1,12 @@
 package qingcloud
 
 import (
-	"log"
-
+	"github.com/hashicorp/packer/common"
 	"github.com/hashicorp/packer/packer"
 	"github.com/mitchellh/multistep"
+	"github.com/yunify/qingcloud-sdk-go/config"
+	qc "github.com/yunify/qingcloud-sdk-go/service"
+	"log"
 )
 
 var _ packer.Builder = &Builder{}
@@ -27,6 +29,34 @@ func (b *Builder) Prepare(raws ...interface{}) ([]string, error) {
 
 // Run ...
 func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packer.Artifact, error) {
+	cfg, err := config.New(b.config.APIKey, b.config.APISecret)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := qc.Init(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	// Setup
+	state := new(multistep.BasicStateBag)
+	state.Put("config", b.config)
+	state.Put("client", client)
+	state.Put("ui", ui)
+
+	// Build
+	steps := []multistep.Step{
+		new(stepCreateInstance),
+		// Run
+		// ...
+	}
+
+	// Run
+	b.runner = common.NewRunner(steps, b.config.PackerConfig, ui)
+	b.runner.Run(state)
+
+	// Output
 	return nil, nil
 }
 
--- qingcloud/step_create_instance.go
+++ qingcloud/step_create_instance.go
@@ -1,1 +1,35 @@
 package qingcloud
+
+import (
+	"github.com/hashicorp/packer/packer"
+	"github.com/mitchellh/multistep"
+	qc "github.com/yunify/qingcloud-sdk-go/service"
+)
+
+type stepCreateInstance struct {
+}
+
+func (s *stepCreateInstance) Run(state multistep.StateBag) multistep.StepAction {
+	ui := state.Get("ui").(packer.Ui)
+	c := state.Get("config").(Config)
+	client := state.Get("client").(*qc.QingCloudService)
+
+	ui.Say("Create Instance...")
+	instanceClient, err := client.Instance(c.Zone)
+	if err != nil {
+		ui.Error(err.Error())
+	}
+
+	// Fix: 主动造成一个错误
+	_, err = instanceClient.RunInstances(&qc.RunInstancesInput{})
+	if err != nil {
+		ui.Error(err.Error())
+	}
+
+	return multistep.ActionContinue
+}
+
+// Cleanup TODO:
+func (s *stepCreateInstance) Cleanup(state multistep.StateBag) {
+
+}
